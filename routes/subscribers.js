3ec13adba13eeadd226f34567c29d0d9db80464d
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-09 10:38:47 +0800

    add create

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -18,8 +18,18 @@ router.get('/:id', (req, res) => {
 })
 
 // Creating one
-router.post('/', (req, res) => {
-  
+router.post('/', async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel
+  })
+
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (error) {
+    res.status(400).json({ message: error.message })    
+  }
 })
 
 // Updating one
